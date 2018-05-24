9b571e0a80bd96328740d275a294481b4efecc29
batflyer <alexander@batflyer.net>
2018-05-24 14:54:31 -0500

    Adding tests.py and rnlptests directory.

---
+++ rnlp/tests/rnlptests/test_textprocessing.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2017-2018 StARLinG Lab
+
+import sys
+import unittest
+
+sys.path.append('./')
+
+from rnlp import textprocessing
+
+class removePunctuationTest(unittest.TestCase):
+    
+    def test_removePunctuation_1(self):
+        self.assertTrue(True)
+
